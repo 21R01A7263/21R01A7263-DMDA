9cb1d20f3b8f1dfcad9dc79a7fd872f3327c209e
Gaurav Sai Teja <128503039+21R01A7263@users.noreply.github.com>
2023-12-22 11:50:55 +0530

    Week10C

--- decisiontree.R
+++ decisiontree.R
@@ -1,16 +1,7 @@
 library(party)
-# Print some records from data set readingSkills.
-# Create the input data frame. input.dat <- readingSkills[c(1:105),]
-# Create the input data frame
 input.dat <- readingSkills[c(1:105),]
-# Give the chart file a name.
 png(file = "decision_tree.png")
-# Create the tree.
 output.tree <- ctree( nativeSpeaker ~ age + shoeSize + score, data =
                         input.dat)
-# Plot the tree.
 plot(output.tree)
-# Save the file.
 dev.off()
-# Plot the tree.
-plot(output.tree)
